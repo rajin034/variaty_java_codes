9296779a915a026ee52f798e82e264275bda56f8
rajin034 <43627925+rajin034@users.noreply.github.com>
2022-06-29 12:07:50 -0500

    added

--- src/varietyExample.java
+++ src/varietyExample.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 public class varietyExample {
     public static void main(String[] args) {
         mapExamples();
+        filterExamples();
+        aggregatingOperationsExamples();
 
     }
 
@@ -19,10 +21,31 @@ public class varietyExample {
         List<String> doubled = ints.stream().map(i -> i + "_" + i).collect(Collectors.toList());
         System.out.println(doubled);
         System.out.println("Mapping with Complex function");
-        List<Integer> pows = pow2.stream().map(d-> {
-            String str= Double.toString(d);
+        List<Integer> pows = pow2.stream().map(d -> {
+            String str = Double.toString(d);
             return str.length();
         }).collect(Collectors.toList());
+        System.out.println(pows);
 
+    }
+
+    private static void filterExamples() {
+        List<String> words = Arrays.asList("tst", "one", "two", "three", "four", "five", "rotator", "deified");
+        System.out.println("1. Simple Filtering");
+        List<String> longWords = words.stream()
+                .filter(s -> s.length() > 3).collect(Collectors.toList());
+        System.out.println(longWords);
+        System.out.println("2. Little More Complex Filtering");
+        List<String> palindrome = words.stream().filter(s -> s.equals(new StringBuilder(s).reverse().toString()))
+                .collect(Collectors.toList());
+        System.out.println(palindrome);
+    }
+    private static void aggregatingOperationsExamples(){
+        List<String> words = Arrays.stream("Once upon a midnight dreary while I pondered weak and weary".split("\\s+"))
+                .collect(Collectors.toList());
+        System.out.println("1. Check if all items satisfy a criteria");
+        boolean noWordsLongerThan9 =words.stream().allMatch(w -> w.length()<3);
+        boolean theSame = words.stream().noneMatch(w -> w.length() <3);
+        System.out.println(noWordsLongerThan9);
     }
 }
